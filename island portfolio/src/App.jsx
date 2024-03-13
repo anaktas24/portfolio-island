86647e4adf2c62f746dcce8d12fb4c7af21182db
Ioannis Antoniou <johnantony24@hotmail.com>
2024-03-13 21:14:23 +0100

    navbar

--- island portfolio/src/App.jsx
+++ island portfolio/src/App.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { Route, BrowserRouter as Router, Routes} from "react-router-dom"
+import NavBar from "./components/NavBar"
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <main className="bg-slate-300/20">
+      <Router>
+        <NavBar/>
+        <Routes>
+          <Route path="/" element={'Home'}/>
+          <Route path="/about" element={'About'}/>
+          <Route path="/projects" element={'Projects'}/>
+          <Route path="/contact" element={'Contact'}/>
+        </Routes>
+      </Router>
+    </main>
   )
 }
 
