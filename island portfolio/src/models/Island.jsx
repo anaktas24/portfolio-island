7711a674797b2b60affff718dbdeb3d18c6861bf
Ioannis Antoniou <johnantony24@hotmail.com>
2024-03-21 20:55:07 +0100

    island rotation

--- island portfolio/src/models/Island.jsx
+++ island portfolio/src/models/Island.jsx
@@ -54,6 +54,34 @@ const Island = ({isRotating, setIsRotating, ...props}) => {
     }
   }
 
+  useFrame(() => {
+    if(!isRotating){
+      rotationSpeed.current *= dampingFactor
+      if(Math.abs(rotationSpeed.current) < 0.001){
+        rotationSpeed.current = 0
+      }
+    }else{
+      const rotation = islandRef.current.rotation.y
+      const normalizedRotation = ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI)
+      switch (true) {
+        case normalizedRotation >= 5.45 && normalizedRotation <= 5.85:
+          setCurrentRotation(4)
+          break;
+        case normalizedRotation >= 0.85 && normalizedRotation <= 1.3:
+          setCurrentRotation(3)
+          break;
+        case normalizedRotation >= 2.4 && normalizedRotation <= 2.6:
+          setCurrentRotation(2)
+          break;
+        case normalizedRotation >= 4.25 && normalizedRotation <= 4.75:
+          setCurrentRotation(1)
+          break;
+        default:
+          setCurrentStage(null)
+      }
+    }
+  })
+
   useEffect(() => {
     document.addEventListener('pointerdown', handlePointerDown)
     document.addEventListener('pointerup', handlePointerUp)
