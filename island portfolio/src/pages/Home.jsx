cbf94b4bd404ed193a27cb90413dd4c5ae885f4d
Ioannis Antoniou <johnantony24@hotmail.com>
2024-03-22 08:40:52 +0100

    homeinfo

--- island portfolio/src/pages/Home.jsx
+++ island portfolio/src/pages/Home.jsx
@@ -5,9 +5,6 @@ import Island  from '../models/Island'
 import Sky from '../models/Sky'
 import Bird from '../models/Bird'
 import Plane from '../models/Plane'
-{/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
-POPUP
-</div> */}
 
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false)
@@ -43,6 +40,10 @@ const Home = () => {
 
   return (
     <section className="w-full h-screen relative">
+      <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        {currentStage && <HomeInfo currentStage={currentStage}/> }
+      </div>
+
       <Canvas
         className={`w-full h-screen bg-transparent $(isRotating ? 'cursor-grabbing' : 'cusor-grab'}`}
         camera={{near: 0.1, far:1000}}>
